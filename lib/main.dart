b89bfcbbe864c1eda821543adb43c2e448158e49
Aya <ayahame99@gmail.com>
2025-09-23 09:12:50 +0300

    initializing project with splash Screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:contact_app/app_theme.dart';
+import 'package:contact_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ContactApp());
+}
+
+class ContactApp extends StatelessWidget {
+  const ContactApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen()
+      },
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
